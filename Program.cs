49bdc5af7149fd511314c8b4454bac5baef64805
unknown <kate.mudrow@snow.edu>
2025-09-25 21:29:44 -0600

    greeting

--- Program.cs
+++ Program.cs
@@ -3,3 +3,14 @@ Date:
 Lab: Lab 5 Mastermind*/
 
 Console.Clear();
+
+string welcome = @"
+Welcome to the game Mastermind!
+I have set a secret code with4 letters between
+'k' and 't'and they are arranged in a specific order. 
+Your job is to guess which letters and put them in the right order.
+
+Ready? Press any key to continue:";
+
+Console.WriteLine($"{welcome}");
+Console.ReadKey(true);
